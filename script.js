db675ccf5dae344f64b33372d1a0f498c3248b4a
Jase-G <j.gutziet@share.epsb.ca>
2024-10-18 12:10:23 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById("btn1").addEventListener("click", btn1Phrase);
+
+function btn1Phrase() {
+  var btn1Phrase = "So far so good.";
+  alert(btn1Phrase);
+}
+
+document.getElementById("btn2").addEventListener("click", btn2Phrase);
+
+function btn2Phrase() {
+  var btn2Phrase = "Back to the drawing room.";
+  alert(btn2Phrase);
+}
+
+document.getElementById("btn3").addEventListener("click", btn3Phrase);
+
+function btn3Phrase() {
+  var btn3Phrase = "Speak of the devil.";
+  alert(btn3Phrase);
+}
+
+document.getElementById("btn4").addEventListener("click", btn4Phrase);
+
+function btn4Phrase() {
+  var btn4Phrase = "Barking up the wrong tree.";
+  alert(btn4Phrase);
+}
+
+document.getElementById("btn5").addEventListener("click", btn5Phrase);
+
+function btn5Phrase() {
+  var btn5Phrase = "Costs an arm and a leg.";
+  alert(btn5Phrase);
+}
